5ff3418e2cc7c1ab71070cd1295d2c2c5001eaa2
Thomas M <thomasjmontanaro@gmail.com>
2025-10-07 21:58:42 -0400

    modified:   script.js 	modified:   style.css 	modified:   script.js

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 'use strict';
 
-// Selecting elements
-const score0 = document.querySelector('#score--0');
-const score1 = document.getElementById('score--1');
+// Selecting Elements
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1'); //Faster but same
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+// Starting Conditions
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
+
+//Rolling Dice Functionality
+btnRoll.addEventListener('click', function () {
+  // 1. Generate a random dice roll
+  const dice = Math.trunc(Math.random() * 6) + 1;
+
+  // 2. Display dice roll
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+  // 3. Check for a rolled 1: if true, switch to next player
+});
